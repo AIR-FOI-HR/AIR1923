79ffbd4a3a21e96c72349004bdb6c3115e344f10
lvukovic1 <lvukovic1@foi.hr>
2019-11-15 15:39:36 +0100

    Izrada početne forme Kreirana je početna forma za aplikaciju. Dodana su dva gumba koja će u daljnjoj implementaciji povezivati prijavu na aplikaciju i registraciju na samu aplikaciju.

--- mGradnja/app/src/main/java/com/mgradnja/MainActivity.java
+++ mGradnja/app/src/main/java/com/mgradnja/MainActivity.java
@@ -3,12 +3,20 @@ package com.mgradnja;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button btnPrijava;
+    private Button btnRegistracija;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnPrijava = findViewById(R.id.btnPrijava);
+        btnRegistracija = findViewById(R.id.btnRegistracija);
     }
 }
