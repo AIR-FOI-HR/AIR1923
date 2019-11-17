9cc0c74aa459361f33c1b21e051e77deb70380c1
mvecenaj <mvecenaj@foi.hr>
2019-11-17 15:03:24 +0100

    Izrada login screena Izrađen je i dizajniran screen namijenjen za prijavu u aplikaciju. Povezan je s gumbom PRIJAVA kako bi se navigiralo na potreban screen.

--- mGradnja/app/src/main/java/com/mgradnja/MainActivity.java
+++ mGradnja/app/src/main/java/com/mgradnja/MainActivity.java
@@ -2,7 +2,9 @@ package com.mgradnja;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -18,5 +20,19 @@ public class MainActivity extends AppCompatActivity {
 
         btnPrijava = findViewById(R.id.btnPrijava);
         btnRegistracija = findViewById(R.id.btnRegistracija);
+
+        btnPrijava.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openLoginActivity();
+            }
+        });
+    }
+
+    public void openLoginActivity(){
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
     }
 }
+
+
