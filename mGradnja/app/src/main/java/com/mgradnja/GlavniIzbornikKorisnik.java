a9c263fb21c4ce379f6985b0cfc7d125d34254aa
mvecenaj <mvecenaj@foi.hr>
2019-11-27 14:14:02 +0100

    Izrada glavnog izbornika za korisnika Izrađen je glavni izbornik koji se prikazuje korisniku nakon uspješne prijave. Izrađene su ikone kao i padajući izbornici sa županijama i kategorijama.

---
+++ mGradnja/app/src/main/java/com/mgradnja/GlavniIzbornikKorisnik.java
@@ -0,0 +1,14 @@
+package com.mgradnja;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GlavniIzbornikKorisnik extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_glavni_izbornik_korisnik);
+    }
+}
