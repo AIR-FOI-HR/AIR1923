9ad24a678dcfbae51b00088d999751daa69846b0
vmatisic <vmatisic@foi.hr>
2019-12-10 15:54:45 +0100

    Popravak otvaranja aktivnosti vezano za korisniči račun i usklađivanje grana.

--- mGradnja/app/build/generated/not_namespaced_r_class_sources/debug/r/androidx/constraintlayout/widget/R.java
+++ mGradnja/app/build/generated/not_namespaced_r_class_sources/debug/r/androidx/constraintlayout/widget/R.java
@@ -74,7 +74,6 @@ public final class R {
         private id() {}
 
         public static final int bottom = 0x7f0a0023;
-<<<<<<< HEAD
         public static final int end = 0x7f0a004d;
         public static final int gone = 0x7f0a005c;
         public static final int invisible = 0x7f0a0076;
@@ -88,21 +87,6 @@ public final class R {
         public static final int start = 0x7f0a00ce;
         public static final int top = 0x7f0a00eb;
         public static final int wrap = 0x7f0a011a;
-=======
-        public static final int end = 0x7f0a004b;
-        public static final int gone = 0x7f0a005a;
-        public static final int invisible = 0x7f0a0073;
-        public static final int left = 0x7f0a0078;
-        public static final int packed = 0x7f0a008e;
-        public static final int parent = 0x7f0a0090;
-        public static final int percent = 0x7f0a0093;
-        public static final int right = 0x7f0a009b;
-        public static final int spread = 0x7f0a00c1;
-        public static final int spread_inside = 0x7f0a00c2;
-        public static final int start = 0x7f0a00c7;
-        public static final int top = 0x7f0a00de;
-        public static final int wrap = 0x7f0a0106;
->>>>>>> 758da4f46929f01d118584aab96ae27fc804d1ca
     }
     public static final class styleable {
         private styleable() {}
